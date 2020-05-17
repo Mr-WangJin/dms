a5b758c02da43901ddc803b4eab6cf701c6838d1
wangjl-f <wangjl-f@glodon.com>
2020-05-17 21:41:49 +0800

    初始化界面

--- mainWin.py
+++ mainWin.py
@@ -4,17 +4,21 @@ import sys
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from ui.mainWin import *
 
+
+class MainWin(QMainWindow):
+    def __init__(self, parent=None):
+        super(MainWin, self).__init__(parent)
+        ui = Ui_MainWindow()
+        ui.setupUi(self)
+
+        self.setWindowTitle('装修管理')
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
-    mainform = QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(mainform)
-
+    mainform = MainWin()
     mainform.show()
     # 系统exit()方法确保应用程序干净的退出
     # 的exec_()方法有下划线。因为执行是一个Python关键词。因此，exec_()代替
     sys.exit(app.exec_())
-
-
-
